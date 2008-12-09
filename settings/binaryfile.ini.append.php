b8748040c7caf4643efba33d1793cdacc0510966
Paul Borgermans <paul.borgermans@gmail.com>
2008-12-09 13:36:16 +0000

    - added application/octet-stream as a catcher for Office 2007 file types (which are not yet known in eZ Publish)
    git-svn-id: http://svn.projects.ez.no/eztika@3 444cf039-8e29-4a61-9d8a-64bcb8e67d3e

--- settings/binaryfile.ini.append.php
+++ settings/binaryfile.ini.append.php
@@ -17,7 +17,11 @@ MetaDataExtractor[application/vnd.oasis.opendocument.presentation]=ezmulti
 MetaDataExtractor[application/vnd.oasis.opendocument.spreadsheet]=ezmulti
 MetaDataExtractor[application/vnd.oasis.opendocument.formula]=ezmulti
 MetaDataExtractor[application/zip]=ezmulti
-
+#Office 2007 word file, but not yet supported in eZP4.0,eZP4.1
+MetaDataExtractor[application/vnd.openxmlformats-officedocument.wordprocessingml.document]=ezmulti
+#octet-stream is the default, and in eZP4.O Office 2007 files are assigned this mime type
+#If tika senses a known format like the Office 2007 types, it will treat them well
+MetaDataExtractor[application/octet-stream]=ezmulti
 [MultiHandlerSettings]
 
 #Change the path to the eztika shell script, which probably needs editing as well
